68fbf1e36bcae4792fdb51179978411fe99802f6
Eric Liu <eyliubit@gmail.com>
2020-04-26 16:02:49 -0400

    modify name and add readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# Shorts or Pants
+
+A weather app telling you whether you should wear shorts or pants today.
+
+## Development
+
+- `yarn dev` to start [webpack-dev-server](https://webpack.js.org/configuration/dev-server/) and go to [localhost:8080](http://localhost:8080)
--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const wrapper = document.getElementById('container');
-wrapper ? ReactDOM.render(<h1>Shirt or Pants</h1>, wrapper) : false;
+wrapper ? ReactDOM.render(<h1>Shorts or Pants</h1>, wrapper) : false;
