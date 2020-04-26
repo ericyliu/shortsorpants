b8fb837230a45d264521e63c015767fb33639682
Eric Liu <eyliubit@gmail.com>
2020-04-26 19:20:42 -0400

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,38 +2,47 @@ const CopyPlugin = require('copy-webpack-plugin');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const WebpackShellPlugin = require('webpack-shell-plugin');
 
-module.exports = {
-  entry: './web/index.js',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
+module.exports = (env, argv) => {
+  config = {
+    entry: './web/index.js',
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
           },
-        ],
-      },
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: 'html-loader',
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: './web/index.html',
+        filename: './index.html',
+      }),
+      new CopyPlugin([{ from: './web/assets', to: 'assets' }]),
     ],
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: './web/index.html',
-      filename: './index.html',
-    }),
-    new CopyPlugin([{ from: './web/assets', to: 'assets' }]),
-    new WebpackShellPlugin({
-      onBuildEnd: ['babel-watch server/index.js'],
-    }),
-  ],
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+  };
+
+  if (argv.mode === 'development') {
+    config.plugins.push(
+      new WebpackShellPlugin({
+        onBuildEnd: ['babel-watch server/index.js'],
+      })
+    );
+  }
+
+  return config;
 };
